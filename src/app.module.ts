126a84a8484a8ab4f772579ff9753ad548dfe1d9
csagastegui59 <csagastegui59@gmail.com>
2023-11-20 14:00:03 -0500

    Add module config implementing class providers and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,51 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TeacherController } from './Entities/Teacher/teacher.controller';
+import { TeacherService } from './Entities/Teacher/teacher.service';
+import { PrismaService } from './prisma.service';
+import { CourseController } from './Entities/Course/course.controller';
+import { CourseService } from './Entities/Course/course.service';
+import { StudentController } from './Entities/Student/student.controller';
+import { StudentService } from './Entities/Student/student.service';
+import { EnrollmentController } from './Entities/Enrollment/enrollment.controller';
+import { EnrollmentService } from './Entities/Enrollment/enrollment.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingInterceptor } from './logging.interceptor';
+import { ErrorInterceptor } from './error.interceptor';
+import { HealthController } from './health.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      // envFilePath: '.production.env',
+    }),
+  ],
+  controllers: [
+    AppController,
+    TeacherController,
+    CourseController,
+    StudentController,
+    EnrollmentController,
+    HealthController,
+  ],
+  providers: [
+    AppService,
+    TeacherService,
+    PrismaService,
+    CourseService,
+    StudentService,
+    EnrollmentService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ErrorInterceptor,
+    },
+  ],
 })
 export class AppModule {}
